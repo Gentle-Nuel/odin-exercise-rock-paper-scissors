bba3c6a51d02f0bf5520870c9e24596b77c80c2c
Gentle-Nuel <embaeri@gmail.com>
2023-05-09 19:06:10 +0100

    add function to play a round of game and log the winner

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ function getComputerChoice() {
 }
 // console.log(getComputerChoice());
 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        console.log(`Round ${round}: It's a tie!`);
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) {
+        playerScore++;
+        console.log(`Round ${round}: You win! ${playerSelection} beats ${computerSelection}`);
+    } else {
+        computerScore++;
+        console.log(`Round ${round}: You lost! ${computerSelection} beats ${playerSelection}`);
+    }
+    round++;
+}
