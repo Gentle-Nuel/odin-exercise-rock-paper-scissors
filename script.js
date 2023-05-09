487595e7b46801d71f6b5e6b0b996c2a8d24428f
Gentle-Nuel <embaeri@gmail.com>
2023-05-09 10:26:34 +0100

    add function for computer to pick random option from rock paper scissors

---
+++ script.js
@@ -0,0 +1,7 @@
+function getComputerChoice() {
+    const options = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * options.length);
+    return options[randomIndex];
+}
+// console.log(getComputerChoice());
+
