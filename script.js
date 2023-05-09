f94941b614cc7942a38c14e174a6dba0f95be52c
Gentle-Nuel <embaeri@gmail.com>
2023-05-09 10:28:11 +0100

    add variables for player and computer scores

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let playerScore = 0;
+let computerScore = 0;
+let round = 1;
+
 function getComputerChoice() {
     const options = ['rock', 'paper', 'scissors'];
     const randomIndex = Math.floor(Math.random() * options.length);
